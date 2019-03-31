f477de699d7fd1413965d215d14b35f7ce8bb9ba
Yashwant Kumar <yashwant.nie@gmail.com>
2019-04-01 00:01:57 +0530

    Update precache-manifest.139967771de742c3c8152e88340db9a1.js

--- precache-manifest.139967771de742c3c8152e88340db9a1.js
+++ precache-manifest.139967771de742c3c8152e88340db9a1.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "47c2a09ace322fcfdc5e83dcd7b8045d",
-    "url": "/static/media/DP.47c2a09a.jpg"
+    "url": "static/media/DP.47c2a09a.jpg"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "98f80dec5804006bf22b",
-    "url": "/static/js/main.52fc64f2.chunk.js"
+    "url": "static/js/main.52fc64f2.chunk.js"
   },
   {
     "revision": "3e360627a3c92f7fcfd3",
-    "url": "/static/js/2.7a31f00c.chunk.js"
+    "url": "static/js/2.7a31f00c.chunk.js"
   },
   {
     "revision": "98f80dec5804006bf22b",
-    "url": "/static/css/main.2ddd304c.chunk.css"
+    "url": "static/css/main.2ddd304c.chunk.css"
   },
   {
     "revision": "94d3108591e867f3378eddd4b34d3459",
-    "url": "/index.html"
+    "url": "index.html"
   }
 ];
