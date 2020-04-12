4f96932b468a747aa5487e6d57b35859178c8d18
Yashwant Kumar <Yashwant_Kumar@intuit.com>
2020-04-12 15:50:22 +0530

    docs folder

---
+++ docs/precache-manifest.83f72c46dd621197e15bc8e94278d3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06397e05edd87009e2c5",
+    "url": "/static/css/main.c1063af6.chunk.css"
+  },
+  {
+    "revision": "06397e05edd87009e2c5",
+    "url": "/static/js/main.2989d104.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b833db9397fc2171d34",
+    "url": "/static/js/2.92f6fcad.chunk.js"
+  },
+  {
+    "revision": "47c2a09ace322fcfdc5e83dcd7b8045d",
+    "url": "/static/media/DP.47c2a09a.jpg"
+  },
+  {
+    "revision": "90465615e255299541fb9e028c075a9c",
+    "url": "/index.html"
+  }
+];
